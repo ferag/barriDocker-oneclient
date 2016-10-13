9b1360372045c45796010cacc02946050cf35cf6
Fernando Aguilar <ferag@live.com>
2016-10-13 10:26:26 +0200

    Added running script

---
+++ lifewatch_run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+mkdir -p /onedata/input
+mkdir -p /onedata/output
+
+ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o ro /onedata/input || exit 1
+ONECLIENT_AUTHORIZATION_TOKEN="$OUTPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$OUTPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/output || exit 1
+
+echo Start at $(date)
+
+INPUTDIR="/onedata/input/$INPUT_ONEDATA_SPACE/$INPUT_PATH"
+OUTPUTDIR="/onedata/output/$OUTPUT_ONEDATA_SPACE/$OUTPUT_PATH"
+
+
+WORKDIR=$MESOS_SANDBOX
+#WORKDIR="$OUTPUTDIR"
+
+# Extract input
+echo Extracting input
+#tar xvfz "$INPUTDIR/delft3d_repository.tar.gz" --no-same-owner -C "$WORKDIR" || exit 1
+#cd "$WORKDIR"/delft3d_repository/06_delwaq || exit 2
+
+cd ./delft3d_repository/examples/06_delwaq || exit 1
+
+echo Editting $D3D_PARAM with value $D3D_VALUE
+
+if [ ! -z $D3D_PARAM ]; then
+ sed -i "s/.* ; $D3D_PARAM/$D3D_VALUE ; $D3D_PARAM/g" /home/aguilarf/IFCA/INDIGO/WP2/demo/delft3d_repository/examples/06_delwaq/com-tut_fti_waq.inp || exit 1
+ grep "; $D3D_PARAM" /home/aguilarf/IFCA/INDIGO/WP2/demo/delft3d_repository/examples/06_delwaq/com-tut_fti_waq.inp
+fi
+
+echo Run test
+# Run Rscript
+./run_delwaq.sh || exit 1
+tar cvfz output.tgz *
+
+# Collect output
+mkdir -p "$OUTPUTDIR"/
+cp $(echo "$OUTPUT_FILENAMES" | tr ',' ' ' ) "$OUTPUTDIR"/  || exit 1
+
+echo End at $(date)
+
+sleep 5
+
+umount /onedata/input || exit 1
+umount /onedata/output || exit 1
