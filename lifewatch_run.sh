f1e47164f202856028026bafece0b57e5e3cf880
Fernando Aguilar <ferag@live.com>
2016-11-05 13:10:12 +0100

    Add log.txt and rw

--- lifewatch_run.sh
+++ lifewatch_run.sh
@@ -3,7 +3,7 @@
 mkdir -p /onedata/input
 mkdir -p /onedata/output
 
-ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o ro /onedata/input || exit 1
+ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/input || exit 1
 ONECLIENT_AUTHORIZATION_TOKEN="$OUTPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$OUTPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/output || exit 1
 
 echo Start at $(date)
@@ -37,7 +37,7 @@ fi
 
 echo Run test
 # Run Rscript
-./run_delwaq.sh > log.txt || exit 1
+./run_delwaq.sh > log.txt  || exit 1
 
 sleep 3
 
